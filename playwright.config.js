13ce60498f832556a1aceb678e4803a6f21fc6ce
ZharukDanil <danil.qerew@gmail.com>
2025-10-12 14:24:42 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -23,8 +23,8 @@ export default defineConfig({
     }
   ],
   webServer: {
-    command: 'npm run start',
+    command: 'npx http-server -p 3000',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
