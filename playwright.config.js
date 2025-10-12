3b3faf60cbbb65bcf14eab007ea4ed81a1b73177
ZharukDanil <danil.qerew@gmail.com>
2025-10-12 14:27:39 +0300

    Заменён http-server на node server.js для CI

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     }
   ],
   webServer: {
-    command: 'npx http-server -p 3000',
+    command: 'npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
   },
